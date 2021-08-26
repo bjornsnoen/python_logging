2d52a27cf9f665fec68e30d23424ac24dc649a33
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-08-26 21:40:38 +0200

    (chore) Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="brbcoffee_logging",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_namespace_packages(include=["brbcoffee.*"]),
     python_requires=">=3.5",
     dependency_links=[],
